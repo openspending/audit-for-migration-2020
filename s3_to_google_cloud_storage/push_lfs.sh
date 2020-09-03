278573179a19c32f67057fc6918f8e056e456973
Sebastien Lavoie <sebastien.lavoie@datopian.com>
2020-09-03 11:53:08 -0500

    [push_lfs,script][s]: Fix syntax error in Bash commands
    * Commands were missing the final backslash before `cd ..`.

--- s3_to_google_cloud_storage/push_lfs.sh
+++ s3_to_google_cloud_storage/push_lfs.sh
@@ -6,103 +6,103 @@ cd presupuesto-mexico-2008-2019 && \
     git lfs track "*.csv" && \
     git add . && \
     git commit -m "Add tracked CSV files" && \
-    git lfs push origin master &&
+    git lfs push origin master && \
     cd ..
 
 cd paraguay-2017 && \
     git lfs track "*.csv" && \
     git add . && \
     git commit -m "Add tracked CSV files" && \
-    git lfs push origin master &&
+    git lfs push origin master && \
     cd ..
 
 cd south-africa-national-estimates-2015-2019 && \
     git lfs track "*.csv" && \
     git add . && \
     git commit -m "Add tracked CSV files" && \
-    git lfs push origin master &&
+    git lfs push origin master && \
     cd ..
 
 cd costa-rica-presupuesto-egresos-2017 && \
     git lfs track "*.csv" && \
     git add . && \
     git commit -m "Add tracked CSV files" && \
-    git lfs push origin master &&
+    git lfs push origin master && \
     cd ..
 
 cd costa-rica-presupuesto-ingresos-2018 && \
     git lfs track "*.csv" && \
     git add . && \
     git commit -m "Add tracked CSV files" && \
-    git lfs push origin master &&
+    git lfs push origin master && \
     cd ..
 
 cd presupuesto-de-uruguay && \
     git lfs track "*.csv" && \
     git add . && \
     git commit -m "Add tracked CSV files" && \
-    git lfs push origin master &&
+    git lfs push origin master && \
     cd ..
 
 cd south-africa-provincial-estimates-2018-2019 && \
     git lfs track "*.csv" && \
     git add . && \
     git commit -m "Add tracked CSV files" && \
-    git lfs push origin master &&
+    git lfs push origin master && \
     cd ..
 
 cd costa-rica-presupuesto-egresos-2018 && \
     git lfs track "*.csv" && \
     git add . && \
     git commit -m "Add tracked CSV files" && \
-    git lfs push origin master &&
+    git lfs push origin master && \
     cd ..
 
 cd costa-rica-presupuesto-ingresos-2019 && \
     git lfs track "*.csv" && \
     git add . && \
     git commit -m "Add tracked CSV files" && \
-    git lfs push origin master &&
+    git lfs push origin master && \
     cd ..
 
 cd costa-rica-presupuesto-egresos-2019 && \
     git lfs track "*.csv" && \
     git add . && \
     git commit -m "Add tracked CSV files" && \
-    git lfs push origin master &&
+    git lfs push origin master && \
     cd ..
 
 cd costa-rica-presupuesto-ingresos-2020 && \
     git lfs track "*.csv" && \
     git add . && \
     git commit -m "Add tracked CSV files" && \
-    git lfs push origin master &&
+    git lfs push origin master && \
     cd ..
 
 cd republica-dominicana-gastos-economica-2014-2020 && \
     git lfs track "*.csv" && \
     git add . && \
     git commit -m "Add tracked CSV files" && \
-    git lfs push origin master &&
+    git lfs push origin master && \
     cd ..
 
 cd costa-rica-presupuesto-egresos-2020 && \
     git lfs track "*.csv" && \
     git add . && \
     git commit -m "Add tracked CSV files" && \
-    git lfs push origin master &&
+    git lfs push origin master && \
     cd ..
 
 cd croatia-budget-spending && \
     git lfs track "*.csv" && \
     git add . && \
     git commit -m "Add tracked CSV files" && \
-    git lfs push origin master &&
+    git lfs push origin master && \
     cd ..
 
 cd presupuesto-mexico-2008-2019 && \
     git lfs track "*.csv" && \
     git add . && \
     git commit -m "Add tracked CSV files" && \
-    git lfs push origin master &&
+    git lfs push origin master && \
     cd ..
